2a9cb82266fd69a42684668a44e44015f26622d8
Arther Koestler <arthur.koestler@gmail.com>
2019-02-09 15:14:35 -0500

    it works! (Prev next buttons, etc)

--- thepage.js
+++ thepage.js
@@ -1,6 +1,6 @@
  'use strict';
 
- var CUSTOM_SEARCH_BASEURL="https://www.googleapis.com/customsearch/v1?";
+ var CUSTOM_SEARCH_BASEURL="https://www.googleapis.com/customsearch/v1";
  //var API_KEY = 'AIzaSyC_O1nIR5CCFlFA_58Tav61WJ-Pk5gB6xQ';
  var API_KEY = 'AIzaSyDeNVlJNs_PDGt7ZmlUbbSfzi4e4veyUkE';
  var SEARCH_ENGINE_ID = '016537884841717004065:ydmz8gvwhvq';
@@ -8,22 +8,22 @@
 
 function mkURL(base,dict) {
   var urlParams = new URLSearchParams();
-  for (var k in dict) {
-    urlParams.append(k, dict[k]);
-  }
-  return base + '?' + urlParams;
+    for (var k in dict) {
+	if (dict[k]) {
+	    urlParams.append(k, dict[k]);
+	}
+    }
+    return base + '?' + urlParams;
 }
 
 function mkLink(href,text) {
   return '<a href=' + href + '>' + text + '</a>';
 }
 
-function Search(q,showFunc,elem,cont) {
+function Search(q,elem,cont) {
     this.elem = elem;
     this.cont = cont;
-    this.showFunc = showFunc;
     this.q = q;
-    this.searchType = searchType;
 
     this.totalResults = this.nextIdx = this.prevIdx = this.itemsOnPage = null;
     this.idx = 0;
@@ -34,40 +34,41 @@ function Search(q,showFunc,elem,cont) {
 
 
 Search.prototype.run = function(idx) {
-  var self = this;
-  var req = new XMLHttpRequest();
+    var self = this;
+    var req = new XMLHttpRequest();
+    var res = null;
+    var url = mkURL(CUSTOM_SEARCH_BASEURL,{'searchType': this.searchType,
+					   'start': idx,
+					   'q': this.q,
+					   'key': API_KEY,
+					   'cx': SEARCH_ENGINE_ID});
+    this.elem.innerHTML = '';
+    req.responseType = "json";
+    req.open("GET",url);
+    req.send();
+    this.done = false;
+    req.onreadystatechange = function(e) {
+	if (req.readyState == 4 && req.status == 200) {
+	    res = req.response;
+	    console.log(res);
+	    
+	    self.itemsOnPage = res.items.length;
+	    self.idx = res.queries.request[0].startIndex - 1;
+	    self.totalResults = res.queries.request[0].totalResults;
 
-  var url = mkURL(CUSTOM_SEARCH_BASEURL,{'searchType': this.searchType,
-                                     'startIdx': idx,
-                                     'q': this.q,
-                                     'key': API_KEY,
-                                     'cx': SEARCH_ENGINE_ID});
-  this.elem.innerHTML = '';
-  req.responseType = "json";
-  req.open("GET",url);
-  req.send();
-  this.done = false;
-  req.onreadystatechange = function(e) {
-      self.done = true;
-       console.log(req.response);
-
-       self.itemsOnPage = req.response.items.length;
-       self.idx = req.response.queries.request.startIndex;
-       self.totalResults = req.response.queries.request.totalResults;
-
-       try {
-         self.nextIdx = req.response.queries.nextPage.startIdx;
-         self.prevIdx = req.response.queries.prevPage.startIdx; // CHECK THIS???
-       } catch {
-
-       }
-
-       for (var i=0; i < self.itemsOnPage; i++) {
-         self.elem.innerHTML += self.showFunc(req.response.items[i]);
-       }
-
-       self.cont();
-  };
+	    if (self.totalResults > self.idx+self.itemsOnPage) {
+		self.nextIdx = res.queries.nextPage[0].startIndex;
+	    }
+	    if (self.idx > 0) {
+		self.prevIdx = res.queries.previousPage[0].startIndex;
+	    }
+	    for (var i=0; i < self.itemsOnPage; i++) {
+		self.elem.innerHTML += self.show(res.items[i]);
+	    }
+	    self.done = true;
+	    self.cont();
+	}
+    };
 }
 
 Search.prototype.hasPrev = function() {
@@ -87,17 +88,18 @@ Search.prototype.prevPage = function() {
 }
 
 function MetaSearch(q,elem) {
-  this.q = q;
-  this.elem = elem;
-  this.totalResults = null;
-  this.normalSearch = new Search(this.q,this.showNormal,this.elem.getElementById('right'),this.barrier);
-  this.imgSearch = new ImgSearch(this.q,this.showImg,this.elem.getElementById('left'),this.barrier);
+    this.q = q;
+    this.elem = elem;
+    this.totalResults = null;
+    this.normalSearch = new Search(this.q,this.elem.getElementById('right'),this.barrier.bind(this));
+    this.imgSearch = new ImgSearch(this.q,this.elem.getElementById('left'),this.barrier.bind(this));
+    this.mkBottomNav();
 }
 
 MetaSearch.prototype.barrier = function() {
   if (this.imgSearch.done && this.normalSearch.done) {
     // Put async things in here
-    this.makeBottomNav()
+    this.refreshBottomNav()
   }
 }
 
@@ -118,31 +120,32 @@ MetaSearch.prototype.prevPage = function() {
   this.imgSearch.prevPage();
 }
 
-MetaSearch.prototype.makeBottomNav = function(hasPrev,hasNext) {
-  // We need to wait for the requests to complete before we can do this
-  var html = 0;
-  if (this.hasPrev()) {
-    html += '<button id=nextBtn>next</button>';
-  }
-  if (this.hasNext()) {
-    html += '<button id=prevBtn>prev</button>';
-  }
-  this.elem.getElementById('bottom').innerHTML = html;
+MetaSearch.prototype.refreshBottomNav = function() {
+    this.elem.getElementById('nextBtn').style.visibility = this.hasNext() ? 'visible' : 'hidden';
+    this.elem.getElementById('prevBtn').style.visibility = this.hasPrev() ? 'visible' : 'hidden';
+}
+
+MetaSearch.prototype.mkBottomNav = function() {
+    var self = this;
   this.elem.getElementById('nextBtn').onclick = function() {
-    this.nextPage();
+    self.nextPage();
   }
   this.elem.getElementById('prevBtn').onclick = function() {
-    this.prevPage();
+    self.prevPage();
   }
 }
 
 Search.prototype.show = function(it) {
-    return '<div class="result">'+'<h3>'+link(it.link,it.title)+'</h3>'+'<h5>'+it.snippet+'</h5></div>';
+    return '<div class="result">'+'<h3>'+mkLink(it.link,it.title)+'</h3>'+'<h5>'+it.snippet+'</h5></div>';
 }
 
 function ImgSearch() {
-  this.searchType = 'image';
+    this.searchType = 'image';
+    Search.apply(this,arguments);
 }
+
+ImgSearch.prototype = Object.create(Search.prototype);
+ImgSearch.prototype.constructor = ImgSearch;
 
 ImgSearch.prototype.show = function(it) {
     return '<div class="imgresult"><a href='+it.link+'><img src='+it.link+'></img></a><h5>'+ it.title + '</h5><h6>' + it.displayLink + '</h6>' + '</div>';
